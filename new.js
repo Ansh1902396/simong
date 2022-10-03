ef3c3800c2025d816333dc8b51b4e05517b022e6
Ansh1902396 <84223519+Ansh1902396@users.noreply.github.com>
2022-10-03 22:51:26 +0530

    Update new.js

--- new.js
+++ new.js
@@ -15,6 +15,14 @@ $(document).keypress(function() {
   }
 });
 
+$(".btn-start").click(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 $(".btn").click(function() {
 
   var userChosenColour = $(this).attr("id");
